0b2faed3336bf47da29bb614c39f4a5ac767e533
Rojan Karn <rokarn12@gmail.com>
2024-03-14 18:49:54 -0400

    Update testbench

--- FIR-Filter-Design/RTL_Code/pipelined_testbench.sv
+++ FIR-Filter-Design/RTL_Code/pipelined_testbench.sv
@@ -4,36 +4,27 @@
 
 module pipelined_testbench();
 	`include "my_filter_parameters.sv"
-		
-	// define local parameters
 	
 	// NUM_SAMPLES = 27
-	localparam int INP_WIDTH = 16;
-	localparam int OUTP_WIDTH = 16;
-	localparam int SIM_TIME = 1000; // Simulation time in ns
-	localparam int NUM_FREQ_TESTS = 30;
-	localparam int pi = 3.141592654;
-	localparam real scale_factor = 1 / real'(2**14);
-	localparam int CLK_PERIOD_NS = 100;
 	
-	// define local signals
+	// tb signals
 	logic clk;
 	logic signed[15:0] inp;
 	logic signed[39:0] outp;
 	logic signed[39:0] outp_max;
 	
 	real num_samples;
-	real frequency, step_size, mag_dB, rad, s;
+	real magnitude, radians, s;
 	
 	// Instantiate DUT
 	fir_filter dut(.clk(clk), .inp(inp), .outp(outp));
 	
-	// main test
+	// run
 	initial begin
 		#1;
 		clk = 1'b0;
 		inp = 16'b0;
-		rad = 0; s = 0;
+		radians = 0;
 		
 		// start the frequency tests
 		for (int i = 1; i < 27; i++) begin
@@ -51,10 +42,10 @@ module pipelined_testbench();
 				 if (outp > outp_max) outp_max = outp;
 			end
 
-			mag_dB = real'(20) * $log10($itor(outp_max) * (1/real'(2**14)));
+			magnitude = real'(20) * $log10($itor(outp_max) * (1/real'(2**14)));
 			
 			// display maximum output magnitude in dB for each frequency test
-			$display("Clock fraction: %f | Maximum y (dB): %f", real'(real'(i)/ real'(27)), mag_dB);
+			$display("Section: %f | Max Magnitude Out: %f", real'(real'(i)/ real'(27)), magnitude);
 			
 			#1000;
 		end
